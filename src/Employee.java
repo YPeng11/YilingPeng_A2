5d61dbcfb7d1cb603a56d624f4544ab54beeb061
YPeng11 <1306821402@qq.com>
2024-12-03 10:33:36 +0800

    write code for Person class and Employee class
    Part 1: 3 instance variables for Person class, and 2 instance variables for Employee.

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,32 @@
-public class Employee {
+public class Employee extends Person {
+    private String employeeID;
+    private String position; // 添加一个职位属性
 
+    // 默认构造函数
+    public Employee() {
+    }
+
+    // 带参数的构造函数
+    public Employee(String name, int age, String gender, String employeeID, String position) {
+        super(name, age, gender);
+        this.employeeID = employeeID;
+        this.position = position;
+    }
+
+    // Getter 和 Setter
+    public String getEmployeeID() {
+        return employeeID;
+    }
+
+    public void setEmployeeID(String employeeID) {
+        this.employeeID = employeeID;
+    }
+
+    public String getPosition() {
+        return position;
+    }
+
+    public void setPosition(String position) {
+        this.position = position;
+    }
 }
