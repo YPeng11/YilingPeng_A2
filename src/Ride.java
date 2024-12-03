b92fc21492fcdfe2fe3c33ec2fc36d7b06224873
YPeng11 <1306821402@qq.com>
2024-12-03 11:04:51 +0800

    write code for Ride class and Visitor class

--- src/Ride.java
+++ src/Ride.java
@@ -1,3 +1,41 @@
 public class Ride {
+    private String rideName;
+    private int theme;
+    private Employee operator; // 员工类作为操作员
 
+    // 默认构造函数
+    public Ride() {
+    }
+
+    // 带参数的构造函数
+    public Ride(String rideName, int theme, Employee operator) {
+        this.rideName = rideName;
+        this.theme = theme;
+        this.operator = operator;
+    }
+
+    // Getter 和 Setter
+    public String getRideName() {
+        return rideName;
+    }
+
+    public void setRideName(String rideName) {
+        this.rideName = rideName;
+    }
+
+    public int gettheme() {
+        return theme;
+    }
+
+    public void settheme(int theme) {
+        this.theme = theme;
+    }
+
+    public Employee getOperator() {
+        return operator;
+    }
+
+    public void setOperator(Employee operator) {
+        this.operator = operator;
+    }
 }
--- src/Visitor.java
+++ src/Visitor.java
@@ -1,3 +1,32 @@
-public class Visitor {
+public class Visitor extends Person {
+    private String visitorID;
+    private int membershipStatus;
 
+    // 默认构造函数
+    public Visitor() {
+    }
+
+    // 带参数的构造函数
+    public Visitor(String name, int age, String gender, String visitorID, int membershipStatus) {
+        super(name, age, gender); // 调用父类的构造函数
+        this.visitorID = visitorID;
+        this.membershipStatus = membershipStatus;
+    }
+
+    // Getter 和 Setter
+    public String getvisitorID() {
+        return visitorID;
+    }
+
+    public void setvisitorID(String visitorID) {
+        this.visitorID = visitorID;
+    }
+
+    public int getmembershipStatus() {
+        return membershipStatus;
+    }
+
+    public void setmembershipStatus(int membershipStatus) {
+        this.membershipStatus = membershipStatus;
+    }
 }
